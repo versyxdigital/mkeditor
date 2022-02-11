cca64bbad592115de68c896481a90c4a507621c0
Chris Rowles <christopher.rowles@outlook.com>
2022-02-11 20:31:36 +0000

    extend commands

--- app/lib/handlers/command-handler.js
+++ app/lib/handlers/command-handler.js
@@ -1,4 +1,5 @@
 import { KeyMod, KeyCode } from 'monaco-editor/esm/vs/editor/editor.api'
+import { commands, blockAlerts, codeBlocks } from './command/map' 
 
 class CommandHandler
 {
@@ -7,77 +8,19 @@ class CommandHandler
     }
 
     registerAll() {
+        const handle = this
+
+        for (const cmd in commands) {
+            commands[cmd].run = () => handle[cmd]()
+            this.instance.addAction(commands[cmd])
+        }
+
         this.instance.addAction({
             id: 'settings',
             label: 'Open Settings Dialog',
             keybindings: [ KeyMod.CtrlCmd | KeyCode.US_SEMICOLON ],
             run: () => $('#settings').modal('show')
         })
-
-        this.instance.addAction({
-            id: 'bold',
-            label: 'Make Text Bold',
-            keybindings: [ KeyMod.CtrlCmd | KeyCode.KEY_B ],
-            contextMenuGroupId: 'navigation',
-            contextMenuOrder: 1,
-            run: (editor) => this.bold(editor.getModel().getValue())
-        })
-
-        this.instance.addAction({
-            id: 'italic',
-            label: 'Make Text Italic',
-            keybindings: [ KeyMod.CtrlCmd | KeyCode.KEY_I ],
-            contextMenuGroupId: 'navigation',
-            contextMenuOrder: 2,
-            run: (editor) => this.italic(editor.getModel().getValue())
-        })
-
-        this.instance.addAction({
-            id: 'strikethrough',
-            label: 'Make Text Strikethrough',
-            keybindings: [ KeyMod.CtrlCmd | KeyCode.KEY_G ],
-            contextMenuGroupId: 'navigation',
-            contextMenuOrder: 3,
-            run: (editor) => this.strikethrough(editor.getModel().getValue())
-        })
-
-        this.instance.addAction({
-            id: 'unordered-list',
-            label: 'Convert To Unordered List',
-            keybindings: [ KeyMod.CtrlCmd | KeyCode.KEY_1 ],
-            contextMenuGroupId: 'cutcopypaste',
-            contextMenuOrder: 4,
-            run: (editor) => this.unorderedList(editor.getModel().getValueInRange(editor.getSelection()))
-        })
-
-        this.instance.addAction({
-            id: 'ordered-list',
-            label: 'Convert To Ordered List',
-            keybindings: [ KeyMod.CtrlCmd | KeyCode.KEY_2 ],
-            contextMenuGroupId: 'cutcopypaste',
-            contextMenuOrder: 5,
-            run: (editor) => this.orderedList(editor.getModel().getValueInRange(editor.getSelection()))
-        })
-
-        this.instance.addAction({
-            id: 'replace-steps',
-            label: 'Convert Ordered List To Task List',
-            keybindings: [ KeyMod.CtrlCmd | KeyCode.KEY_3 ],
-            contextMenuGroupId: 'cutcopypaste',
-            contextMenuOrder: 6,
-            run: (editor) => this.replaceStepsCmd(editor.getModel().getValueInRange(editor.getSelection()))
-        })
-
-        let blockAlerts = [
-            {type: 'Primary', key: 'P'},
-            {type: 'Secondary', key: 'E'},
-            {type: 'Info', key: 'I'},
-            {type: 'Success', key: 'S'},
-            {type: 'Warning', key: 'W'},
-            {type: 'Danger', key: 'D'},
-            {type: 'Light', key: 'L'},
-            {type: 'Dark', key: 'R'},
-        ]
 
         blockAlerts.forEach((block) => {
             this.instance.addAction({
@@ -92,18 +35,6 @@ class CommandHandler
                 }
             })
         })
-
-        let codeBlocks = [
-            {type: 'Powershell', key: 'P'},
-            {type: 'CLI', key: 'L'},
-            {type: 'CSharp', key: 'C'},
-            {type: 'FSharp', key: 'F'},
-            {type: 'Visual-Basic', key: 'V'},
-            {type: 'Python', key: 'Y'},
-            {type: 'SQL', key: 'S'},
-            {type: 'XML', key: 'X'},
-            {type: 'JSON', key: 'J'}
-        ]
 
         codeBlocks.forEach((block) => {
             this.instance.addAction({
@@ -153,7 +84,7 @@ class CommandHandler
         this.replaceSelection(this.getSelectionValue().replace(/^[a-zA-Z]+?/gm, (match) => `${++i}. ${match}`))
     }
 
-    replaceSteps() {
+    orderedListToTaskList() {
         let i = 0
         this.replaceSelection(this.getSelectionValue().replace(/^([0-9]+)\.\s+(?!\[)/gm, (match) => `${++i}. [ ] ${match}`))
     }
@@ -172,10 +103,6 @@ class CommandHandler
 
     displayMedia(alt, uri) {
         this.replaceSelection(`![${alt}](${uri})`)
-    }
-
-    replaceStepsCmd(content) {
-        this.replaceSelection(content.replace(/^([0-9]+)\.\s+(?!\[)/gm, '$1. [ ] '))
     }
 
     replaceSelection(replacement) {
