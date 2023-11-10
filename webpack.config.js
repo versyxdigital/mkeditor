998e33106b909d28ebdf705d51dbb8970574ea6d
Chris Rowles <christopher.rowles@outlook.com>
2023-11-10 19:07:21 +0000

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,86 +5,92 @@ const TerserWebpackPlugin = require('terser-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: {
-      mkeditor: [
-        './src/browser/index.ts',
-        './src/browser/assets/scss/index.scss'
-      ]
-    },
-    output: {
-      globalObject: 'self',
-      filename: 'mkeditor.bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    optimization: {
-      minimizer: [new TerserWebpackPlugin({
-        extractComments: true
-      })]
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
-        },
-        {
-          test: /\.scss$/,
-          exclude: /node_modules/,
-          use: [
-              {
-                  loader: 'file-loader',
-                  options: { name: 'mkeditor.bundle.css' }
-              },
-              'sass-loader'
-          ]
-        },
-        {
-          test: /\.ttf$/,
-          type: 'asset/resource'
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['.ts', '.js']
-    },
-    plugins: [
-      new MonacoWebpackPlugin({
-          languages: ['markdown'],
-          features: [
-              'clipboard',
-              'codeAction',
-              'contextmenu',
-              'cursorUndo',
-              'find',
-              'fontZoom',
-              'inPlaceReplace',
-              'indentation',
-              'lineSelection',
-              'links',
-              'multicursor',
-              'quickCommand',
-              'referenceSearch',
-              'wordHighlighter'
-          ]
-      }),
-      new webpack.optimize.LimitChunkCountPlugin({
-          maxChunks: 1
-      }),
-      new CopyWebpackPlugin({
-        patterns: [
-            { from: './src/shared/assets' },
-            {
-              from: './src/shared/assets/img/favicon.ico',
-              to: './favicon.ico'
-            },
-            { from: './src/browser/views'}
-        ]
-      })
+  mode: 'production',
+  entry: {
+    mkeditor: [
+      './src/browser/index.ts',
+      './src/browser/assets/scss/index.scss'
     ]
+  },
+  output: {
+    globalObject: 'self',
+    filename: 'mkeditor.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    minimizer: [new TerserWebpackPlugin({
+      extractComments: true
+    })]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'mkeditor.bundle.css' }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.ttf$/,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new MonacoWebpackPlugin({
+      languages: ['markdown'],
+      features: [
+        'bracketMatching',
+        'caretOperations',
+        'clipboard',
+        'codeAction',
+        'contextmenu',
+        'cursorUndo',
+        'find',
+        'folding',
+        'fontZoom',
+        'inPlaceReplace',
+        'indentation',
+        'lineSelection',
+        'linesOperations',
+        'links',
+        'multicursor',
+        'quickCommand',
+        'referenceSearch',
+        'wordHighlighter',
+        'wordOperations',
+        'wordPartOperations'
+      ]
+    }),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/shared/assets' },
+        {
+          from: './src/shared/assets/img/favicon.ico',
+          to: './favicon.ico'
+        },
+        { from: './src/browser/views'}
+      ]
+    })
+  ]
 };
