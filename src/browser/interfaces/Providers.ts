19c13a98a6664cc46c17569e0fa005a7befa1b67
Chris Rowles <christopher.rowles@outlook.com>
2023-11-14 04:34:04 +0000

    Clean up providers

--- src/browser/interfaces/Providers.ts
+++ src/browser/interfaces/Providers.ts
@@ -2,16 +2,16 @@ import { Bridge } from '../lib/Bridge';
 import { Command } from '../lib/Command';
 import { Settings } from '../lib/Settings';
 
-
-export interface BridgeProviders {
-  settings: Settings | null;
+export interface Providers {
+  bridge: Bridge | null;
   command: Command | null;
+  settings: Settings | null;
+}
+
+export interface BridgeProviders extends Omit<Providers, 'bridge'> {
   [key: string]: unknown | null;
 }
 
-export interface EditorProviders {
-  bridge: Bridge|null;
-  command: Command|null;
-  settings: Settings|null;
+export interface EditorProviders extends Providers {
   [key: string]: unknown | null;
 }
