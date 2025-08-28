a5b5d44a213fee2c7f9a5f7f33a236bbb980ebcb
Chris Rowles <c.rowles@sentrybay.com>
2025-08-28 17:54:32 +0100

    Fix the preview.scrollTop/line=1 scroll sync issue

--- src/browser/extensions/ScrollSync.ts
+++ src/browser/extensions/ScrollSync.ts
@@ -72,8 +72,15 @@ const ScrollSync = async (line: number, preview: HTMLElement) => {
       Math.max(1, preview.scrollTop + scrollTo),
     );
 
-    // TODO fix scroll top margin properly
-    if (preview.scrollTop <= 125) preview.scrollTop = 0;
+    // If we're at the top of the editor then force scroll top
+    if (line === 1) preview.scroll({ top: 0 });
+
+    // console.log({
+    //   scrollTop: preview.scrollTop,
+    //   scrollTo: scrollTo,
+    //   currLine: line,
+    //   nextLine: next?.line ?? null,
+    // });
 
     return resolve(preview);
   });
